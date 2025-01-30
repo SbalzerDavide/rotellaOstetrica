bdd3bb242dffbd8af0665c305df47dc26946ff70
SbalzerDavide <sbalzerdavide@hotmail.it>
2025-01-30 23:03:58 +0100

    update default and translations

--- transloco.config.ts
+++ transloco.config.ts
@@ -3,6 +3,7 @@ import {TranslocoGlobalConfig} from '@jsverse/transloco-utils';
 const config: TranslocoGlobalConfig = {
   rootTranslationsPath: 'src/assets/i18n/',
   langs: [ 'en', 'it' ],
+  defaultLang: 'it',
   keysManager: {}
 };
     
